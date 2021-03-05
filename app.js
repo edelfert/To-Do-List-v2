dc7efe5cc8430e6a43e2431144a4c0f39af8e134
Eddie <github.towage@aleeas.com>
2021-03-04 17:50:40 -0800

    Add items to to do list onto database

--- app.js
+++ app.js
@@ -34,34 +34,49 @@ const code = new Item ({
 
 const defaultItems = [dishes, laundry, code];
 
-Item.insertMany(defaultItems, function(err){
-  if (err) {
-    console.log(err)
-  } else {
-    console.log("Success!")
-  }
-})
+
 
 
 app.get("/", function(req, res) {
 
 
 
-  res.render("list", {listTitle: "Today", newListItems: items});
+  Item.find({}, function(err, foundItems){
+
+    if (foundItems.length === 0 ) {
+      Item.insertMany(defaultItems, function(err){
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("Success!")
+        }
+      })
+
+      res.redirect('/');
+    } else {
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+
+
+  });
+
+
 
 });
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+const item = new Item ({
+  name: itemName
+})
+
+item.save();
+
+res.redirect("/")
+
+
 });
 
 app.get("/work", function(req,res){
