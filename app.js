6fe779633d93863005001b8a2d60dded353789d6
Eddie <github.towage@aleeas.com>
2021-03-09 18:41:39 -0800

    add delete feature

--- app.js
+++ app.js
@@ -79,6 +79,20 @@ res.redirect("/")
 
 });
 
+app.post("/delete", function(req,res){
+  const checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId, function(err){
+    if (err) {
+      console.log("error");
+
+    } else {
+      console.log("success!");
+      res.redirect("/")
+    }
+  })
+})
+
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
